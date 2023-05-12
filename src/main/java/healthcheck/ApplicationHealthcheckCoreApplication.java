c57ed65e094fdc23c0cbb90f3bd5d0039493573a
CQA4186_hca <ashutosh.palai@theelixrlabs.com>
2023-05-12 12:54:30 +0530

    Initial commit

---
+++ src/main/java/healthcheck/ApplicationHealthcheckCoreApplication.java
@@ -0,0 +1,13 @@
+package healthcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationHealthcheckCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationHealthcheckCoreApplication.class, args);
+    }
+
+}
